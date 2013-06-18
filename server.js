b05f2b39b9ea439de62f694eca1ae32bb43f7b33
Jason May <jason.a.may@gmail.com>
2013-06-17 22:20:36 -0400

    get some simple working views up in here!

--- server.js
+++ server.js
@@ -1,30 +1,12 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-  , net = require('net')
+var express = require('express')
+  , app     = express()
+  , server  = require('http').createServer(app)
+  , io      = require('socket.io').listen(server)
+  , net     = require('net')
   , jstream = require('JSONStream')
-  , config = require('./config');
+  , config  = require('./config');
 
-function error(status, msg) {
-  var err = new Error(msg);
-  err.status = status;
-  return err;
-}
-
-app.use('/data', function (req, res, next) {
-  next();
-});
-
-app.use(app.router);
-
-app.use(function(err, req, res, next) {
-  res.send(err.status || 500, { error: err.message });
-});
-
-app.use(function(req, res) {
-  res.send(404, { error: "File not found" });
-});
-
+// Terminal stream tracking stuff
 var streams = {};
 var manager_parser = new jstream.parse();
 var socket = new net.Socket({type: 'unix'});
@@ -72,6 +54,31 @@ manager_parser.on('data', function (data) {
       }
     }
   }
+});
+
+// Express stuff
+
+function error(status, msg) {
+  var err = new Error(msg);
+  err.status = status;
+  return err;
+}
+
+app.use(express.logger());
+
+app.use('/', express.static(__dirname + '/public'));
+app.use('/data', function (req, res, next) {
+  next();
+});
+
+app.use(app.router);
+
+app.use(function(err, req, res, next) {
+  res.send(err.status || 500, { error: err.message });
+});
+
+app.use(function(req, res) {
+  res.send(404, { error: "File not found" });
 });
 
 app.get('/data/streams', function(req, res, next) {
