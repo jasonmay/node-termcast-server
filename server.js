92fe19d3f53f5d0a2dbeb77cad5d5e3f72e041ca
Jason May <jason.a.may@gmail.com>
2013-06-17 20:13:06 -0400

    get a dummy server up and running

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-var ffi   = require('ffi');
-var createVT = require('./build/Release/vtchanges');
-var vtchanges = createVT(80, 24);
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-var p = vtchanges.process("one\r\n\033[0;32mtwo\033[m\r\nt\033[1;34mhre\033[me");
-console.log(require('util').inspect(p, false, null));
+function error(status, msg) {
+  var err = new Error(msg);
+  err.status = status;
+  return err;
+}
 
-vtchanges.finish();
+app.use('/data', function (req, res, next) {
+  next();
+});
+
+app.use(app.router);
+
+app.use(function(err, req, res, next) {
+  res.send(err.status || 500, { error: err.message });
+});
+
+app.use(function(req, res) {
+  res.send(404, { error: "File not found" });
+});
+
+app.get('/data/streams', function(req, res, next) {
+  res.send({fortytwo: 42});
+});
+
+if (!module.parent) {
+  server.listen(4000);
+  console.log('Web server listening on port 4000');
+}
